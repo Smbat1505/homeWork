59b8a9919acef4bd71e631b19bcb8cf56ba70bc8
Sem Antonian <smbat1584@gmail.com>
2023-08-18 07:19:08 +0400

    update homework

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -47,8 +47,14 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    // const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter(us => {
+       return  us.address.city === 'Los Angeles' ;
+      // if (us.address.city === 'Los Angeles') return us
+    })
+
     setCurrentUsers({ myFriends: filteredUsers });
+    console.log(filteredUsers)
   };
 
   return (
